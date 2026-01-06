120c0ebe4cd1c7209c7432833dcce3e7a50d2fe1
mayur <motingemayur@gmail.com>
2026-01-06 17:27:19 +0530

    first commit

---
+++ data/insta_posts.js
@@ -0,0 +1,77 @@
+// DATA FOR PHOTOGRAPHY
+        const photoData = [
+            { 
+                img: "images/instaposts/mayur_m_photography/post1.jpg", 
+                cap: "...", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DDwqMdtTVc4/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_photography/post2.jpg", 
+                cap: "...", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DFCNsO0IS0n/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_photography/post3.jpg", 
+                cap: "...", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DIeFlzbsBSQ/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_photography/post4.jpg", 
+                cap: "...", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DJth1i6zEYR/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_photography/post5.jpg", 
+                cap: "Bramha Kamal", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DM3MVUSsFrs/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_photography/post6.jpg", 
+                cap: "Greenery, Evening, and Calm...", 
+                url: "https://www.instagram.com/mayur_m_photography/p/DNyLaOJ5pc6/"
+            }
+
+
+
+
+        ];
+
+        // DATA FOR WRITINGS
+        const writingData = [
+            { 
+                img: "images/instaposts/mayur_m_writings/post1.jpg", 
+                cap: "दृढता | Tenacity",
+                url: "https://www.instagram.com/mayur_m_writings/p/DMEzfunIfZ6/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_writings/post2.jpg", 
+                cap: "The College...", 
+                url: "https://www.instagram.com/mayur_m_writings/p/DMJ-ZfNo-ns/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_writings/post3.jpg", 
+                cap: "The School... The school is the birthplace of sweet childhood memories and the starting point for endless dreams. It's presented as a priceless and essential stage of life, where young minds, like blooming buds, begin to unfurl and discover new directions and possibilities. In short, it highlights the school's role in shaping a child's entire future.", 
+                url: "https://www.instagram.com/mayur_m_writings/p/DMpMMUDJtOU/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_writings/post4.jpg", 
+                cap: "Happy FRIENDSHIP Day to all of my Friends...", 
+                url: "https://www.instagram.com/mayur_m_writings/p/DM4YPS9oOtl/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_writings/post5.jpg", 
+                cap: "My deepest gratitude to @ashwin.kleem for bringing the epic of Mahavtaar Narshimha to the screen with such devotion and power. It was more than a film; it was a truly divine experience that stirred the soul. Thank you for this unforgettable cinematic and spiritual journey!", 
+                url: "https://www.instagram.com/mayur_m_writings/p/DPZPR5oiMRI/"
+            },
+            { 
+                img: "images/instaposts/mayur_m_writings/post6.jpg", 
+                cap: "Dhurandhar is more than just a movie; it is a powerful reminder of the sacrifices made for our freedom. Some stories stay with you long after the lights come on, and this experience was truly unforgettable.", 
+                url: "https://www.instagram.com/mayur_m_writings/p/DSzj7WZiL_b/"
+            }
+
+
+
+
+
+            
+        ];
